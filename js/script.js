91bf2dc21bd41264314075dc61b8e893d9a58b9a
Elias Verdun02 <eleliasds@gmail.com>
2022-08-01 17:47:02 -0300

    Desafio 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,45 @@
-let number = parseInt(prompt("Ingrese un numero"))
+//Declaracion de variables
+let apellido
+let notas
+let texto = 'Promedios Finales:'
 
-for(let i = 0; i < number; i++){
-    console.log(`Hola N°${i + 1}`)
+//Declaracion de funciones
+const promediar =(notas)=> notas/3
+
+const aprobacion = () =>{
+
+    let promedioFinal = promediar(notas).toFixed(2)
+
+    if(promedioFinal >= 7){
+        return `El alumno ${apellido} Aprobó con: ${promedioFinal}`
+    } else if(promedioFinal < 7) {
+        return `El alumno ${apellido} Desaprobo con: ${promedioFinal}`
+    } else {
+        return `Ingresaste mal un dato`
+    }
 }
+
+const mostrar = ()=>{
+    console.log(texto)
+}
+
+//Comienzo del programa
+const empezar =()=>{
+    alert('A continuacion vamos a exhibir el promedio final de nuestros alumnos. Por favor ingresar apellido y sus 3 notas correspondientes')
+
+    for(let i = 0;i < 3;i++){
+    
+    apellido = prompt(`Porfavor ingresar apellido del Alumno n°${i + 1}`)
+
+    notas = 0
+    for(let i = 0; i < 3; i++){
+        let nota = parseInt(prompt(`${apellido} digame la nota de la evaluacion N°${i + 1}`))
+        notas += nota
+    }
+
+    texto += ` ${aprobacion(notas)}`
+}
+}
+
+
+
