6f016a587020a9f0075cf3e5ba75507ee9699822
Elias Verdun02 <eleliasds@gmail.com>
2022-08-11 13:59:40 -0300

    Pre-entrega

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 //Declaracion de variables
 let alumnos = []
 
+
+
 class Alumno{
     constructor(nombre,notas,promedio,estado){
         this.nombre = nombre
@@ -15,22 +17,21 @@ class Alumno{
 }
 
 //Declaracion de funciones
+
+const mostrarInfo=(alumno)=>{
+    return `
+    Alumno: ${alumno.nombre} 
+    Notas: ${alumno.notas.join(', ')} 
+    Promedio Final: ${alumno.promedio}
+    Estado: ${alumno.estado}`
+}
+
 const promediar =(notas)=> {
     let notasTotales = 0
     for(let nota of notas){
         notasTotales += nota
     }
     return notasTotales / 3
-}
-
-const mostrar = ()=>{
-    for(alumno of alumnos){
-        console.log(`
-         Alumno: ${alumno.nombre} 
-         Notas: ${alumno.notas.join(', ')} 
-         Promedio Final: ${alumno.promedio}
-         Estado: ${alumno.estado}`)
-    }
 }
 
 const aprobacion =(promedioFinal)=>{
@@ -45,12 +46,43 @@ const aprobacion =(promedioFinal)=>{
     
 }
 
+//Funciones de interaccion
+const mostrar = ()=>{
+    for(alumno of alumnos){
+        console.log(mostrarInfo(alumno))
+    }
+}
+
+const mostrarAlumno =(nombre)=>{
+    let alumno = alumnos.find(alumno=> alumno.nombre === nombre)
+    console.log(`${mostrarInfo(alumno)}
+    ${alumno.mensaje()}`)
+}
+
+const mostrarAprobados =()=>{
+    let aprobados = alumnos.filter(alumno=> alumno.estado == 'Aprobado')
+
+    aprobados.forEach((alumno)=>{
+        console.log(mostrarInfo(alumno))
+    })
+} 
+
+const mostrarDesaprobados =()=>{
+    let aprobados = alumnos.filter(alumno=> alumno.estado == 'Desaprobado')
+
+    aprobados.forEach((alumno)=>{
+        console.log(mostrarInfo(alumno))
+    })
+}
+
+
+
 
 //Comienzo del programa
 const empezar =()=>{
     alert('A continuacion vamos a exhibir el promedio final de nuestros alumnos. Por favor ingresar su Nombre y sus 3 notas correspondientes')
 
-    for(let i = 0; i < 3;i++){
+    for(let i = 0; i < 5;i++){
     
     let apellido = prompt(`Porfavor ingresar Nombre del Alumno n°${i + 1}`)
 
@@ -60,7 +92,7 @@ const empezar =()=>{
         notas.push(nota)
     }
 
-    let promedio = parseInt(promediar(notas).toFixed(2))
+    let promedio = parseFloat(promediar(notas).toFixed(2))
     let estado = aprobacion(promedio)
 
     alumnos.push(new Alumno(apellido,notas,promedio,estado))
